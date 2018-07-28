a1e822aaf302314126153de4fc727291be7642a6
Greifrut <artur.bunko@gmail.com>
2018-07-28 22:00:24 +0300

    new links

--- advert-vue/src/App.vue
+++ advert-vue/src/App.vue
@@ -45,11 +45,11 @@
       return {
         drawer: false,
         links: [
-          {
-            title: 'Login',
-            icon: 'lock',
-            url: '/login'
-          }
+          {title: 'Login', icon: 'lock', url: '/login'},
+          {title: 'Registration', icon: 'face', url: '/registration'},
+          {title: 'Orders', icon: 'bookmark_border', url: '/orders'},
+          {title: 'New ad', icon: 'note_add', url: '/new'},
+          {title: 'My ads', icon: 'list', url: '/list'}
         ]
       }
     }
