c258ad2be10e7df5372549890301d672fe3013ee
Greifrut <artur.bunko@gmail.com>
2018-08-01 20:35:55 +0300

    object ads

--- advert-vue/src/components/Home.vue
+++ advert-vue/src/components/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Home</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-container fluid>
+      <v-layout row>
+        <v-flex xs12>
+          <v-carousel>
+            <v-carousel-item></v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container>
+
+    </v-container>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
       return {
-
+        ads: [
+          {
+            title: 'First ads',
+            description: 'Description',
+            promo: false,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            id: '1'
+          },
+          {
+            title: 'Second ads',
+            description: 'Description',
+            promo: true,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            id: '2'
+          },
+          {
+            title: 'Third ads',
+            description: 'Description',
+            promo: true,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            id: '3'
+          }
+        ]
       }
     }
   }
