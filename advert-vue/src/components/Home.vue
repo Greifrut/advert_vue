77d95a4297afc38779c9629024efa4d70910c79d
Greifrut <artur.bunko@gmail.com>
2018-08-01 20:45:29 +0300

    card ads

--- advert-vue/src/components/Home.vue
+++ advert-vue/src/components/Home.vue
@@ -4,13 +4,45 @@
       <v-layout row>
         <v-flex xs12>
           <v-carousel>
-            <v-carousel-item></v-carousel-item>
+            <v-carousel-item
+            v-for="ad in ads"
+            :key="ad.id"
+            :src="ad.imageSrc"
+            ></v-carousel-item>
           </v-carousel>
         </v-flex>
       </v-layout>
     </v-container>
-    <v-container>
+    <v-container grid-list-lg>
+      <v-layout row wrap>
+        <v-flex
+          xs12
+          sm6
+          md4
+          v-for="ad in ads"
+          :key="ad.id"
+        >
+          <v-card>
+            <v-card-media
+              :src="ad.imageSrc"
+              height="200px"
+            ></v-card-media>
 
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">{{ad.title}}</h3>
+                <div>{{ad.description}}</div>
+              </div>
+            </v-card-title>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat >Open</v-btn>
+              <v-btn raised class="primary" >Buy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
