02497f53f565afd8da2aa90ed926e8300f7aa65a
Greifrut <artur.bunko@gmail.com>
2018-08-07 19:08:55 +0300

    refresh_spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'signup/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'refresh', controller: :refresh, action: :create
 end
