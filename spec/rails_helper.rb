ff64154a874536dd2130f9eb031bca1682b1fec1
Greifrut <artur.bunko@gmail.com>
2018-08-07 19:23:58 +0300

    update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
+  config.include FactoryBot::Syntax::Methods
 
   config.infer_spec_type_from_file_location!
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  config.include ResponseHelper
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
