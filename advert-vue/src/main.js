65379fec9d1cf1a99bc6d4f37dc533d5d410b04b
Greifrut <artur.bunko@gmail.com>
2018-08-08 21:49:13 +0300

    axios setting

--- advert-vue/src/main.js
+++ advert-vue/src/main.js
@@ -6,16 +6,21 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import store from './store'
+import VueAxios from 'vue-axios'
+import { secureAxiosInstance, plainAxiosInstance } from './backend/axios'
 
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
+Vue.user(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   store,
+  secureAxiosInstance,
+  plainAxiosInstance,
   components: { App },
   template: '<App/>'
 })
