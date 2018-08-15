27146ac94543dac0a9ace30884eff35ee75d10d2
Greifrut <artur.bunko@gmail.com>
2018-08-15 19:20:05 +0300

    add references to ads, user

--- db/migrate/20180815153824_create_ads.rb
+++ db/migrate/20180815153824_create_ads.rb
@@ -1,9 +1,9 @@
 class CreateAds < ActiveRecord::Migration[5.2]
   def change
     create_table :ads do |t|
-      t.string :title
-      t.string :description
-      t.boolean :promo
+      t.string :title, null: false
+      t.string :description, null: false
+      t.boolean :promo, default: false
 
       t.timestamps
     end
