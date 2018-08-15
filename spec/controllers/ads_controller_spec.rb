aceff2c598e76f4ee6f8412f15a5c538f088d071
Greifrut <artur.bunko@gmail.com>
2018-08-15 20:51:32 +0300

    PUT #update with invalid params  render a JSON response with error

--- spec/controllers/ads_controller_spec.rb
+++ spec/controllers/ads_controller_spec.rb
@@ -110,6 +110,15 @@ RSpec.describe AdsController, type: :controller do
       end
     end
 
+    context 'with invalid params' do
+      it 'render a JSON response with error for the ad' do
+        request.cookies[JWTSessions.access_cookie] = @tokens[:access]
+        request.headers[JWTSessions.csrf_header] = @tokens[:csrf]
+        put :update, params: { id: ad.id, ads: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
   end
 
 end
